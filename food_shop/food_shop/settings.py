a945ad3f67166becc00488aa9fbe4dfcd4baebae
Alexey-Sandor <sandoralexey@gmail.com>
2023-10-17 21:28:23 +0300

    Добавил описание локального запуска проекта и разворачивания его в Docker

--- food_shop/food_shop/settings.py
+++ food_shop/food_shop/settings.py
@@ -101,8 +101,8 @@ WSGI_APPLICATION = 'food_shop.wsgi.application'
 
 DATABASES = {
     "default": {
-        "ENGINE": os.getenv("DB_ENGINE", default="django.db.backends.postgresql"),
-        "NAME": os.getenv("DB_NAME", default="food_shop_project_final"),
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DB_NAME", default="pre-final-db"),
         "USER": os.getenv("POSTGRES_USER", default="Admin"),
         "PASSWORD": os.getenv("POSTGRES_PASSWORD", default="Admin"),
         "HOST": os.getenv("DB_HOST", default="127.0.0.1"),
@@ -148,10 +148,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-if DEBUG:
-    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Media files
 
